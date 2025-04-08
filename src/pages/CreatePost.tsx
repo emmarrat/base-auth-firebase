bd870f12dd4231cf8e356665625016ad9f7757c8
Emir <emmarrat@gmail.com>
2025-04-08 20:10:07 +0600

    add firestore

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
-import { Button, TextField, Container, Box } from '@mui/material';
-import { useAuthStore } from '../store/useAuthStore.ts';
-import {axiosApi} from '../axiosApi.ts';
+import React, {useState} from 'react';
+import {Button, TextField, Container, Box} from '@mui/material';
+import {useAuthStore} from '../store/useAuthStore.ts';
+import {useNavigate} from 'react-router-dom';
+import {createPost} from '../api/posts'
 
 export const CreatePost = () => {
   const [content, setContent] = useState('');
-  const { user } = useAuthStore();
+  const navigate = useNavigate();
+  const {user} = useAuthStore();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!user) return;
 
-    await axiosApi.post('/posts.json', {
+    await createPost({
       content,
       userId: user.id,
-      email: user.email,
+      email: user.email || '',
       createdAt: new Date().toISOString(),
     });
     setContent('');
+    navigate('/');
   };
 
   return (
     <Container maxWidth="md">
-      <Box sx={{ mt: 4 }}>
+      <Box sx={{mt: 4}}>
         <form onSubmit={handleSubmit}>
           <TextField
             fullWidth
@@ -32,7 +35,7 @@ export const CreatePost = () => {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           />
-          <Button variant="contained" type="submit" sx={{ mt: 2 }}>
+          <Button variant="contained" type="submit" sx={{mt: 2}}>
             Create Post
           </Button>
         </form>
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export interface IUser {
   email: string | null;
   id: string;
 }
+
+export interface IPost {
+  id: string;
+  content: string;
+  userId: string;
+  email: string;
+  createdAt: string;
+}
